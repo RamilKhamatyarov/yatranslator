a800bac3f25dcd6f5be328a14c90fae8c8f7da77
RamilKhamatyarov <rramil@gmail.com>
2017-04-27 00:13:27 +0300

    Set database and listview activity

--- app/src/main/java/ru/rkhamatyarov/yatranslator/FavoriteActivity.java
+++ app/src/main/java/ru/rkhamatyarov/yatranslator/FavoriteActivity.java
@@ -2,11 +2,152 @@ package ru.rkhamatyarov.yatranslator;
 
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 /**
  * Created by Asus on 25.04.2017.
  */
 
-public class FavoriteActivity extends Activity {
+public class FavoriteActivity extends ListActivity implements View.OnClickListener {
 
+    private final String LOG_TAG = "YaLog";
+
+    private static final String EXTRA_LINE_FROM = "wordsFrom";
+    private static final String EXTRA_LINE_TO = "wordsTo";
+    private static final String EXTRA_LANG_DIR = "langDir";
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+    private Cursor cursor;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "beginning");
+        super.onCreate(savedInstanceState);
+        ListView favoriteWordsView = getListView();
+//        setContentView(R.layout.activity_favorite);
+        Log.d(LOG_TAG, "after getListView()");
+        dbHelper = new DBHelper(this);
+        try {
+            db = dbHelper.getReadableDatabase();
+            Log.d(LOG_TAG, "after getReadableDatabase()");
+            cursor = db.query("FAVORITE_WORDS",
+                            new String[]{"_id", "WordsFrom"},
+                            null, null, null, null, "WordsFrom"+" DESC");
+
+            Log.d(LOG_TAG, "after db.query()");
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(this, R.layout.activity_favorite,
+                    cursor, new String[]{"WordsFrom"},
+                    new int[]{R.id.wordsFrom}, 0);
+            Log.d(LOG_TAG, "favoriteAdapter");
+            favoriteWordsView.setAdapter(favoriteAdapter);
+//            Log.d("ListView", favoriteWordsView.getItemAtPosition(0).toString());
+
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    String str;
+                    do {
+                        str = "";
+                        for (String cn : cursor.getColumnNames()) {
+                            str = str.concat(cn + " = "
+                                    + cursor.getString(cursor.getColumnIndex(cn)) + "; ");
+                        }
+                        Log.d(LOG_TAG, str);
+
+                    } while (cursor.moveToNext());
+                }
+//                cursor.close();
+            } else
+                Log.d(LOG_TAG, "Cursor is null");
+
+        } catch (SQLiteException exc) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        Log.d(LOG_TAG, "before insertRow");
+        insertRow();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+}
+
+    public void insertRow() {
+        //get strings from main activity
+        Log.d(LOG_TAG, "beginning");
+        Intent intent = getIntent();
+        String langDirs = (String) intent.getExtras().get(EXTRA_LANG_DIR);
+        String linesFrom = (String) intent.getExtras().get(EXTRA_LINE_FROM);
+        String linesTo = (String) intent.getExtras().get(EXTRA_LINE_TO);
+        Log.d(LOG_TAG, "after get intent");
+        Log.d(LOG_TAG, linesFrom);
+        ContentValues transValues = new ContentValues();
+        transValues.put("LangDir", langDirs);
+        transValues.put("WordsFrom", linesFrom);
+        transValues.put("WordsTo", linesTo);
+        Log.d(LOG_TAG, "after contentvalues");
+        Log.d(LOG_TAG, transValues.get("WordsFrom").toString());
+        // start database
+        dbHelper = new DBHelper(this);
+        try{
+            db = dbHelper.getWritableDatabase();
+            db.insert("FAVORITE_WORDS", null, transValues);
+            Log.d(LOG_TAG, "after insert");
+        } catch (SQLiteException exc) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+        final static int DB_VER = 1;
+        final static String DB_NAME = "translateYaDB";
+        final String TABLE_NAME = "FAVORITE_WORDS";
+        final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+
+                " (_id INTEGER PRIMARY KEY, "+
+                "LangDir TEXT, " +
+                "WordsFrom TEXT , " +
+                "WordsTo TEXT)";
+        final String DROP_TABLE = "DROP TABLE IF EXISTS "+TABLE_NAME;
+//        final String DATA_FILE_NAME = "data.txt";
+        Context context;
+
+        public DBHelper(Context context) {
+            super(context, DB_NAME, null, DB_VER);
+            this.context = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "create db "+CREATE_TABLE);
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
 }
--- app/src/main/java/ru/rkhamatyarov/yatranslator/MainActivity.java
+++ app/src/main/java/ru/rkhamatyarov/yatranslator/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -24,16 +25,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
 
+    private final String LOG_TAG = "YaLog";
+
     private RelativeLayout mTextMessage;
 
     private String fullLangDir = "en-ru";
     private EditText editText;
     private Button btnLangFrom;
     private Button btnLangTo;
+    private ImageButton btnFavorite;
     private String wordsLine = null;
 
     private final int REQUEST_LANG_FROM = 1;
     private final int REQUEST_LANG_TO = 2;
+
+    private static final String EXTRA_LINE_FROM = "wordsFrom";
+    private static final String EXTRA_LINE_TO = "wordsTo";
+    private static final String EXTRA_LANG_DIR = "langDir";
+    private static final String EXTRA_FULL_LANG_DIR = "fullLangDir";
+
+    private static final String EXTRA_FULL_LANG_FROM = "LangFrom";
+    private static final String EXTRA_FULL_LANG_TO = "LangTo";
+    private static final String EXTRA_FULL_LANG_FROM_IND = "LangFromInd";
+    private static final String EXTRA_FULL_LANG_TO_IND = "LangToInd";
+
 
     public static TextView translatedWords;
 
@@ -46,9 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 case R.id.translate:
 //                    mTextMessage.setText(R.string.title_home);
 //                    editText.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-//                    editText.setText(R.string.title_dashboard);
+                return true;
+                case R.id.navigation_favorites:
                     return true;
                 case R.id.navigation_notifications:
 //                    editText.setText(R.string.title_notifications);
@@ -64,15 +78,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set toolbar
+        // Set toolbar_main
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //Butons in toolbar panel
+        //Butons in toolbar_main panel
         btnLangFrom = (Button) findViewById(R.id.langFrom);
         btnLangFrom.setOnClickListener(this);
 
         btnLangTo = (Button) findViewById(R.id.langTo);
         btnLangTo.setOnClickListener(this);
+
+        btnFavorite = (ImageButton) findViewById(R.id.favoritesBtn);
+        btnFavorite.setOnClickListener(this);
 
         mTextMessage = (RelativeLayout) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -83,6 +100,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editText.addTextChangedListener(this);
 
         translatedWords = (TextView) findViewById(R.id.translated_words);
+//        translatedWords.setText("Hello");
     }
 
     @Override
@@ -94,8 +112,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.langFrom:
                 intent = new Intent(this, LanguageListActivity.class);
                 Bundle mainFromBundle = new Bundle();
-                mainFromBundle.putSerializable("langDir", LanguageDir.FROM);
-                mainFromBundle.putString("fullLangDir", fullLangDir);
+                mainFromBundle.putSerializable(EXTRA_LANG_DIR, LanguageDir.FROM);
+                mainFromBundle.putString(EXTRA_FULL_LANG_DIR, fullLangDir);
                 intent.putExtras(mainFromBundle);
 
                 startActivityForResult(intent, REQUEST_LANG_FROM);
@@ -103,32 +121,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.langTo:
                 intent = new Intent(this, LanguageListActivity.class);
                 Bundle mainToBundle = new Bundle();
-                mainToBundle.putSerializable("langDir", LanguageDir.TO);
-                mainToBundle.putString("fullLangDir", fullLangDir);
+                mainToBundle.putSerializable(EXTRA_LANG_DIR, LanguageDir.TO);
+                mainToBundle.putString(EXTRA_FULL_LANG_DIR, fullLangDir);
                 intent.putExtras(mainToBundle);
 
                 startActivityForResult(intent, REQUEST_LANG_TO);
+                break;
+            case R.id.favoritesBtn:
+                intent = new Intent(this, FavoriteActivity.class);
+                Bundle mainFavorBundle = new Bundle();
+                mainFavorBundle.putString(EXTRA_LANG_DIR, fullLangDir);
+                mainFavorBundle.putString(EXTRA_LINE_FROM, wordsLine);
+                mainFavorBundle.putString(EXTRA_LINE_TO, wordsLine);
+                intent.putExtras(mainFavorBundle);
+
+                startActivityForResult(intent, 1);
                 break;
         }
 
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d("myLogs", "requestCode = " + requestCode + ", resultCode = " + resultCode);
+        Log.d(LOG_TAG, "requestCode = " + requestCode + ", resultCode = " + resultCode);
         String langFromInd = null, langToInd = null;
         if (resultCode == RESULT_OK){
             switch (requestCode) {
                 case REQUEST_LANG_FROM:
                     Bundle bundleFrom = data.getExtras();
-                    String langFrom = bundleFrom.getString("LangFrom");
-                    langFromInd = bundleFrom.getString("LangFromInd");
+                    String langFrom = bundleFrom.getString(EXTRA_FULL_LANG_FROM);
+                    langFromInd = bundleFrom.getString(EXTRA_FULL_LANG_FROM_IND);
 
                     btnLangFrom.setText(langFrom);
                     break;
                 case REQUEST_LANG_TO:
                     Bundle bundleTo = data.getExtras();
-                    String langTo = bundleTo.getString("LangTo");
-                    langToInd = bundleTo.getString("LangToInd");
+                    String langTo = bundleTo.getString(EXTRA_FULL_LANG_TO);
+                    langToInd = bundleTo.getString(EXTRA_FULL_LANG_TO_IND);
 
                     btnLangTo.setText(langTo);
                     break;
@@ -177,10 +205,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         wordsLine = editText.getText().toString();
-        Log.d("onTextChanged", wordsLine);
+        Log.d(LOG_TAG, wordsLine);
         RequestImpl requestImpl = new RequestImpl(MainActivity.this);
         try {
-            Log.d("onTextChanged", fullLangDir);
+            Log.d(LOG_TAG, fullLangDir);
             requestImpl.getRequest(fullLangDir, wordsLine, RequestOptions.translateWords, null);
         }catch (WrongOptionException exc){
             exc.printStackTrace();
